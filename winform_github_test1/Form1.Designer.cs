3e51f085237ff2420a731455930409d8d4498760
fuzhzh <gz123>
2021-09-29 13:58:07 +0800

    V1.3

--- winform_github_test1/Form1.Designer.cs
+++ winform_github_test1/Form1.Designer.cs
@@ -37,7 +37,7 @@
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(241, 21);
             this.textBox1.TabIndex = 0;
-            this.textBox1.Text = "2";
+            this.textBox1.Text = "3";
             // 
             // Form1
             // 
